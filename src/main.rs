2c655a935d725ffc7fe7fbd5ce4db1a49d5b99b3
minho <dnjsalsgh123@gmail.com>
2025-11-14 11:20:43 +0900

    not exist semicolon is return

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,7 @@ fn main() {
     }
     another_function();
     print_labeled_measurement(5, 'h');
+    express();
 }
 
 fn another_function() {
@@ -56,4 +57,12 @@ fn another_function() {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn express() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
 }
