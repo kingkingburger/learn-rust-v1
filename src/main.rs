0eee6822d04bfcf997d77b025286ec782b443d6a
minho <dnjsalsgh123@gmail.com>
2025-11-29 08:57:28 +0900

    can_hold method

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,17 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangl {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangl {
+        width: 60,
+        height: 45,
+    };
 
-    println!("rect is {:?}", rect1.area());
-    println!("rect is {:#?}", rect1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 struct User {
@@ -45,6 +53,10 @@ struct Rectangl {
 impl Rectangl {
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangl) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
