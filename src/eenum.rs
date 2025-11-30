7c996227707f5a0a0147edba6bc26c8ad59a25c7
minho <dnjsalsgh123@gmail.com>
2025-11-30 20:07:42 +0900

    ushow enum struct

--- src/eenum.rs
+++ src/eenum.rs
@@ -25,3 +25,19 @@ struct IpAddr {
 let home = IpAddr::V4(127,0,0,1);
 
 let loopback = IpAddr::V4(String::from("::1"));
+
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct QuitMessage; // 유닛 구조체
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // 튜플 구조체
+struct ChangeColorMessage(i32, i32, i32); // 튜플 구조체
