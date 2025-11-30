99e2c00f1442c2545c45913366df29d9d259583f
minho <dnjsalsgh123@gmail.com>
2025-11-30 19:59:29 +0900

    initialize enum type

--- src/eenum.rs
+++ src/eenum.rs
@@ -1,6 +1,6 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
 
@@ -12,12 +12,16 @@ struct IpAddr {
     address: String,
 }
 
-let home = IpAddr{
-    kind: IpAddrKind::V4,
-    address: String::from("127.0.0.1");
-};
+// let home = IpAddr{
+//     kind: IpAddrKind::V4,
+//     address: String::from("127.0.0.1");
+// };
 
-let loopback = IpAddr{
-    kind: IpAddrKind::V4,
-    address: String::from("::1");
-};
+// let loopback = IpAddr{
+//     kind: IpAddrKind::V4,
+//     address: String::from("::1");
+// };
+
+let home = IpAddr::V4(String::from("127.0.0.1"));
+
+let loopback = IpAddr::V4(String::from("::1"));
