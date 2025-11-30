2bef130540b9212768e2e815dcc0abef95f70de8
minho <dnjsalsgh123@gmail.com>
2025-11-30 19:58:31 +0900

    example enum 1

--- src/eenum.rs
+++ src/eenum.rs
@@ -6,3 +6,18 @@ enum IpAddrKind {
 
 let four = IpAddrKind::V4;
 let six = IpAddrKind::V6;
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+let home = IpAddr{
+    kind: IpAddrKind::V4,
+    address: String::from("127.0.0.1");
+};
+
+let loopback = IpAddr{
+    kind: IpAddrKind::V4,
+    address: String::from("::1");
+};
