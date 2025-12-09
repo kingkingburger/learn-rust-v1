18931fdaf654aefff84a7138cdfac47eb7c74cff
minho <dnjsalsgh123@gmail.com>
2025-12-09 20:43:08 +0900

    struct is default private

--- src/lib.rs
+++ src/lib.rs
@@ -14,21 +14,30 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    // 정대 경로
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // 상대 경로
-    front_of_house::hosting::add_to_waitlist();
-}
-
-fn deliver_order() {}
-
 mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    fn cook_order() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // 호밀(Rye) 토스트를 곁들인 여름철 조식 주문
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // 먹고 싶은 빵 바꾸기
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // 다음 라인의 주석을 해제하면 컴파일되지 않습니다
+    // 계절 과인은 조회나 수정이 허용되지 않습니다.
+    // meal.seasonal_fruit = String::from("blueberries");
 }
