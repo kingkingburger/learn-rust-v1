d95207d001207f66faffc8cc9fd72ef0f0826076
minho <dnjsalsgh123@gmail.com>
2025-12-13 07:45:26 +0900

    get element from vector

--- src/v1/vector_test.rs
+++ src/v1/vector_test.rs
@@ -1,3 +1,17 @@
 pub fn vector_test() {
     let v: Vec<i32> = Vec::new();
+    let mut v2 = VEc::new();
+
+    v2.push(3);
+
+    let v3 = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v3[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v3.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no element"),
+    }
 }
