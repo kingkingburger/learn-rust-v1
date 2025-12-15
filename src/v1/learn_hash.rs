269c27c7f07ced288963163090f26dd2585b3123
minho <dnjsalsgh123@gmail.com>
2025-12-15 18:21:54 +0900

    using for

--- src/v1/learn_hash.rs
+++ src/v1/learn_hash.rs
@@ -8,5 +8,8 @@ pub fn learn_hash() {
 
     let team_name = String::from("Blue");
     let score = scores.get(&team_name).copied().unwrap_or(0);
-    
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
 }
