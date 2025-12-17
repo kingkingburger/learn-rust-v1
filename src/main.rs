d6da9b7a726e1a90985e3f44eaa42542890cd011
minho <dnjsalsgh123@gmail.com>
2025-12-17 20:13:49 +0900

    throw error use Result type

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,9 @@ mod v1;
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("problem opening the file: {:?}", error),
+    };
 }
