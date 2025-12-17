6996984c6655ddc3a695cf5d2f43ddf3a73d3d78
minho <dnjsalsgh123@gmail.com>
2025-12-17 20:19:38 +0900

    short cute: expect

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use crate::v1::learn_hash::learn_hash;
 mod v1;
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt").unwrap();
+    let greeting_file_result = File::open("hello.txt").expect("should include this project");
 }
