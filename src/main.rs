a027ea788b16c468be5f498481ce4a25c61de4b4
minho <dnjsalsgh123@gmail.com>
2025-12-17 20:25:02 +0900

    return more information in call function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{fs::File, io::ErrorKind};
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
+use rand::Error;
 
 use crate::v1::learn_hash::learn_hash;
 
@@ -6,4 +11,20 @@ mod v1;
 
 fn main() {
     let greeting_file_result = File::open("hello.txt").expect("should include this project");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
 }
