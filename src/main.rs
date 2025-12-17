a95898d8a766707242493a5729c15fed7ffe5829
minho <dnjsalsgh123@gmail.com>
2025-12-17 20:18:32 +0900

    short cut: unwrap

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,5 @@ use crate::v1::learn_hash::learn_hash;
 mod v1;
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
-
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("not creating the file {:?}", e),
-            },
-            other_error => {
-                panic!("opening the file: {:?}", other_error)
-            }
-        },
-    };
+    let greeting_file_result = File::open("hello.txt").unwrap();
 }
