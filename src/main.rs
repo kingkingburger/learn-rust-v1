b8b6a56f34d541319391638472596217fdafa20a
minho <dnjsalsgh123@gmail.com>
2025-12-18 14:16:09 +0900

    collect temp

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ use crate::v1::learn_hash::learn_hash;
 mod v1;
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt").expect("should include this project");
+    let greeting_file_result = File::open("hello.txt").expect("should nclude this project");
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
     let mut username_file = File::open("hello.txt")?;
     let mut username = String::new();
-    username_file.read_to_string(&mut username)?
+    username_file.read_to_string(&mut username)?;
     Ok(username)
 }
