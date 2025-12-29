4ea5ef3d0fbc78ff011c137ce70b56cabfac5723
minho <dnjsalsgh123@gmail.com>
2025-12-30 08:33:45 +0900

    update: make tcp server in rust

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use std::{
-    fs::File,
-    io::{self, Read},
-};
-
-use rand::Error;
-
-use crate::v1::learn_hash::learn_hash;
-
-mod v1;
+use std::net::TcpListener;
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt").expect("should nclude this project");
-}
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username_file = File::open("hello.txt")?;
-    let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
-    Ok(username)
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
