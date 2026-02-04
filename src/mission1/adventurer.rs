d378680ca0d2ba7359d39ba4dfb62e103b210000
minho <dnjsalsgh123@gmail.com>
2026-02-04 21:31:07 +0900

    생성자 만들기

--- src/mission1/adventurer.rs
+++ src/mission1/adventurer.rs
@@ -1,13 +1,21 @@
 pub struct Adventurer {
-    name: String,
-    life: i32,
-    gold: i32,
+    pub name: String,
+    pub life: i32,
+    pub gold: i32,
 }
 
 impl Adventurer {
+    pub fn new(name: String, life: i32, gold: i32) -> Adventurer {
+        Adventurer {
+            name: name,
+            life: life,
+            gold: gold,
+        }
+    }
+
     pub fn introduce(&self) {
         println!("hello my name is {}", self.name);
         println!("my life is {}", self.life);
-        println!("have {} gold" {self.gold});
+        println!("have {} gold", self.gold);
     }
 }
