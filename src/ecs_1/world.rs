7edb08b0c31cc883313e5e5cef962b40407a8d07
minho <dnjsalsgh123@gmail.com>
2026-02-10 14:47:46 +0900

    world 객체 만들기

---
+++ src/ecs_1/world.rs
@@ -0,0 +1,24 @@
+struct World {
+    entities: Vec<Entity>,
+    next_id: u32
+}
+
+impl World {
+    fn new() -> Self {
+        World {
+            entities: Vec::new(),
+            next_id: 0,
+        }
+    }
+
+    fn spawn(&mut self) -> Entity {
+        let id = self.next_id;
+        let new_entity = Entity(id);
+        
+        self.entities.push(new_entity);
+
+        self.next_id = id + 1;
+
+        new_entity
+    }
+}
