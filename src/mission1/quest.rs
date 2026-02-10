e9882b91beea26f6e886c0ebac913afd14cd5748
minho <dnjsalsgh123@gmail.com>
2026-02-10 11:32:35 +0900

    update: quest에 자신의 정보를 println! 하는 함수 추가

--- src/mission1/quest.rs
+++ src/mission1/quest.rs
@@ -5,3 +5,19 @@ pub struct Quest {
     pub level: i32,
     pub reward: Item,
 }
+
+impl Quest {
+    pub fn new(name: &str, level: i32, reward: Item) -> Self {
+        Quest {
+            name: name.to_string(),
+            level,
+            reward,
+        }
+    }
+
+    pub fn show_info(&self) {
+        println!("Quest Name: {}", self.name);
+        println!("Quest Level: {}", self.level);
+        println!("Reward Item: {}", self.reward.name);
+    }
+}
