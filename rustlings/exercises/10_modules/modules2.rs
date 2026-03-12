f5b74b39e2a4cebb197020e7f74a89adaa30e591
minho <dnjsalsgh123@gmail.com>
2026-03-12 15:58:46 +0900

    내부에 있는 모듈을 외부로 보내는 'use' 사용해보기

--- rustlings/exercises/10_modules/modules2.rs
+++ rustlings/exercises/10_modules/modules2.rs
@@ -15,7 +15,12 @@ mod delicious_snacks {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
+
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 }
+
+
 
 fn main() {
     println!(
